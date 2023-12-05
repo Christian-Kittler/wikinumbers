d0974ed0142a09ae50cac521364a575a9b251bd6
Christian <christiankittler@gmx.de>
2023-12-05 13:49:24 +0100

    added logic to initialize the game

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 async function readData() {
   const response = await fetch("./src/data.json");
   const articles = await response.json();
+  return articles;
 }
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
+function getRandomArticle(articles) {
+  const index = Math.floor(Math.random() * articles.length);
+  return articles.splice(index, 1)[0];
 }
+
+function insertDataIntoElement(article, index) {
+  const element = document.getElementsByClassName("article").item(index);
+  element.querySelector("h2").innerText = article.title.replaceAll("_", " ");
+  element.querySelector("span").innerText = article.views;
+}
+
+async function initGame(numberOfArticles) {
+  const articles = await readData();
+  for (let i = 0; i < numberOfArticles; ++i) {
+    insertDataIntoElement(getRandomArticle(articles), i);
+  }
+}
+
+initGame(4);
